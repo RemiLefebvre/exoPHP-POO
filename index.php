bbd9e1ca31b2ccda778f62d855d7f9b814066bef
remilefebvre <lefebvre.reminicolas@gmail.com>
2017-10-24 15:42:46 +0200

    prepare MVC

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+
+/*
+** Detail of vehicul
+*/
+if (condition) {
+  # code...
+}
+
+
+
+/*
+** Listing of all vehicule + filtre
+*/
+elseif (condition) {
+  # code...
+}
+
+
+/*
+** Listing of all vehicule
+*/
+else {
+  # code...
+}
+ ?>
